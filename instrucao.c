00b009c9d93ebf13c1d5a7244cfde31f29a98bf4
rafaeldss <rafaeldss199916@gmail.com>
2019-09-25 23:42:00 -0300

    Primeiros exerc­cios resolvidos

---
+++ instrucao.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int c, estaVariavel, q76354, numero;
+    int a;
+
+    printf("Digite um inteiro: ");
+    scanf("%d", &a);
+    printf("Digite outro inteiro: ");
+    scanf("%d", &numero);
+
+    if(numero != 7) {
+        printf("A variavel numero nao e igual a 7\n");
+    }
+    printf("Este e um \nprograma em C\n\n");
+    printf("Este \ne \num \nprograma \nem \nC \n\n");
+    printf("Este \te\t um \tprograma\t em\t C");
+
+}
